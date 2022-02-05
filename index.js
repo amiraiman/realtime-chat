937da08bd712a86f5b89dbe6aad3c38473de8092
Amir Aiman <amiraiman.jj@gmail.com>
2022-02-05 10:04:29 +0800

    Use render template instead of serving static files

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const express = require("express");
 const app = express();
 const server = http.createServer(app);
 
-app.use(express.static(__dirname + "/views"));
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index.html");
+  res.render("index");
 });
 
 server.listen(8080);
