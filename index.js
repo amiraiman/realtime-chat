54e7892272ed0085d3ed87968bf1545c9e9780d2
Amir Aiman <amiraiman.jj@gmail.com>
2022-02-05 11:03:47 +0800

    Serve public folder on /assets route

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const server = http.createServer(app);
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 
+app.use("/assets", express.static(__dirname + "/public"));
+
 app.get("/", (req, res) => {
   res.render("index");
 });
