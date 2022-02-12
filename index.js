7e52acdb5627e1d6568a3b307ba45a256770f108
Amir Aiman <amiraiman.jj@gmail.com>
2022-02-12 08:57:12 +0800

    Add web socket functionality

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const http = require("http");
 const express = require("express");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
+const io = new Server(server);
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 
 app.use("/assets", express.static(__dirname + "/public"));
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+
+  socket.on("new chat", (data) => {
+    io.emit("new chat", data);
+  });
+});
 
 app.get("/", (req, res) => {
   res.render("index");
